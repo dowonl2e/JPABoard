dcd07395e78cfda1e7e0b94f8be370c71c06fc76
dowonl2e <dierwdod@gmail.com>
2022-01-22 13:13:41 +0900

    게시판 요청(Request) DTO

---
+++ BoardWithJPA/src/main/java/com/myapp/board/dto/BoardRequestDTO.java
@@ -0,0 +1,36 @@
+package com.myapp.board.dto;
+
+import com.myapp.board.entity.Board;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 요청(Request) DTO 클래스
+ * @author dowonlee
+ *
+ */
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BoardRequestDTO {
+	
+	private String title;
+	private String content;
+	private String writer;
+	private char noticeYn;
+	private char secretYn;
+	private char deleteYn;
+	
+	public Board toEntity() {
+		return Board.builder()
+				.title(title)
+				.content(content)
+				.writer(writer)
+				.noticeYn(noticeYn)
+				.secretYn(secretYn)
+				.deleteYn(deleteYn)
+				.hits(0)
+				.build();
+	}
+}
