a66d8c074f2896fb6489770e5561ee4dae1c1267
dowonl2e <dierwdod@gmail.com>
2022-01-22 11:32:27 +0900

    전역 예외처리 핸들러

--- BoardWithJPA/src/main/java/com/myapp/board/exception/GlobalExceptionHandler.java
+++ BoardWithJPA/src/main/java/com/myapp/board/exception/GlobalExceptionHandler.java
@@ -1,17 +1,51 @@
 package com.myapp.board.exception;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * 전역 예외 핸들링용 클래스
+ * @author dowonlee
+ *
+ */
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
-
-	@ExceptionHandler(RuntimeException.class)
-	public String handleRunTimeException(final RuntimeException e) {
-		log.error("handleRuntimeException : {}", e.getMessage());
-		return e.getMessage();
+	
+	/*
+     * Developer Custom Exception
+     */
+	@ExceptionHandler(CustomException.class)
+	protected ResponseEntity<ErrorResponse> handleCustomException(final CustomException e){
+        log.error("handleCustomException: {}", e.getErrorCode());
+        return ResponseEntity
+        		.status(e.getErrorCode().getStatus().value())
+        		.body(new ErrorResponse(e.getErrorCode()));
 	}
+	
+	/*
+	 * HTTP 405 Exception
+	 */
+	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+	protected ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(final HttpRequestMethodNotSupportedException e){
+		log.error("handleHttpRequestMethodNotSupportedException {}", e.getMessage());
+		return ResponseEntity
+				.status(ErrorCode.METHOD_NOT_ALLOWED.getStatus().value())
+				.body(new ErrorResponse(ErrorCode.METHOD_NOT_ALLOWED));
+	}
+	
+	 /*
+     * HTTP 500 Exception
+     */
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<ErrorResponse> handleException(final Exception e) {
+        log.error("handleException: {}", e.getMessage());
+        return ResponseEntity
+                .status(ErrorCode.INTERNAL_SERVER_ERROR.getStatus().value())
+                .body(new ErrorResponse(ErrorCode.INTERNAL_SERVER_ERROR));
+    }
 }
